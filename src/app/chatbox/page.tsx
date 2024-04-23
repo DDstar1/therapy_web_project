c6f5ffa39b82b5447f0d122d7b4ab06873e25947
DDstar1 <abhuluimendestiny@gmail.com>
2024-04-23 17:08:58 +0100

    'dsvs'

--- src/app/chatbox/page.tsx
+++ src/app/chatbox/page.tsx
@@ -1,19 +1,31 @@
 import React from "react";
+import Image from "next/image";
 
 function page() {
   return (
     <div>
       <div className="flex">
-        <div>
+        <div className="px-3">
           <div>Admin List</div>
-          <div>John John</div>
+          <div className="bg-gray-300 rounded-md flex items-center">
+            <div>John John</div>
+            <div>
+              <Image
+                className="rounded-full"
+                src="/dfc_logo.png"
+                alt="Example Image"
+                width={50}
+                height={50}
+              />
+            </div>
+          </div>
           <div>John John</div>
           <div>John John</div>
           <div>John John</div>
           <div>John John</div>
         </div>
         <div className="flex-grow relative pl-2">
-          <div className="absolute text-center">Admin Name</div>
+          <div className="absolute text-center w-full">Admin Name</div>
           <div className=" pt-5"></div>
           <div>
             <span className="float-right">How are you</span>.
@@ -22,18 +34,25 @@ function page() {
             <span className="float-left">Im fine</span>.
           </div>
           <div>
-            <span className="float-right"> I want to go to heaven</span>.
+            <span className="float-right">
+              {" "}
+              I heard you could get advice here
+            </span>
+            .
           </div>
           <div>
             {" "}
-            <span className="float-left">Then go</span>.
+            <span className="float-left">Of course, we are here for you</span>.
           </div>
           <div>
             {" "}
-            <span className="float-right">Do you think a rope is ok</span>.
+            <span className="float-right">
+              I have being on abstainance, but its hard
+            </span>
+            .
           </div>
           <div>
-            <span className="float-left">Make it an accident</span>.
+            <span className="float-left">How long?</span>.
           </div>
         </div>
       </div>
