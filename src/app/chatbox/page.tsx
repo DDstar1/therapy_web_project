679d7dc47b699b22b0f4fea0768171544bea4f7d
DDstar1 <abhuluimendestiny@gmail.com>
2024-04-23 18:12:12 +0100

    'sdvsd'

--- src/app/chatbox/page.tsx
+++ src/app/chatbox/page.tsx
@@ -6,8 +6,8 @@ function page() {
     <div>
       <div className="flex">
         <div className="px-3">
-          <div>Admin List</div>
-          <div className="bg-gray-300 rounded-md flex items-center">
+          <div className="text-center">Admin List</div>
+          <div className="bg-gray-300 rounded-md flex items-center text-center p-2">
             <div>John John</div>
             <div>
               <Image
@@ -24,35 +24,25 @@ function page() {
           <div>John John</div>
           <div>John John</div>
         </div>
-        <div className="flex-grow relative pl-2">
+        <div className="flex flex-col relative pl-2">
           <div className="absolute text-center w-full">Admin Name</div>
           <div className=" pt-5"></div>
+          <div className="mine_msg">How are you svsdv sbs sdvs ewv sdv</div>
+          <div className="their_msg">Im fine</div>
+
           <div>
-            <span className="float-right">How are you</span>.
-          </div>
-          <div>
-            <span className="float-left">Im fine</span>.
-          </div>
-          <div>
-            <span className="float-right">
-              {" "}
-              I heard you could get advice here
-            </span>
-            .
+            <span className=""> I heard you could get advice here</span>.
           </div>
           <div>
             {" "}
-            <span className="float-left">Of course, we are here for you</span>.
+            <span className="">Of course, we are here for you</span>.
           </div>
           <div>
             {" "}
-            <span className="float-right">
-              I have being on abstainance, but its hard
-            </span>
-            .
+            <span className="">I have being on abstainance, but its hard</span>.
           </div>
           <div>
-            <span className="float-left">How long?</span>.
+            <span className="">How long?</span>.
           </div>
         </div>
       </div>
