80c00099bcefa0f2e3a53764a2ed85ded3489037
DDstar1 <abhuluimendestiny@gmail.com>
2024-04-25 11:30:10 +0100

    sds

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,15 +1,48 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { MdOutlineDehaze } from "react-icons/md";
 
 function Nav_bar() {
+  const [navOpen, setnavOpen] = useState(false);
+
+  const toggleNavOpen = (index: any) => {
+    setnavOpen(!navOpen);
+  };
   return (
     <nav className="box-border z-40 flex justify-between p-2 overflow-hidden min-w-full fixed top-0 bg-gray-300 flex-grow">
       <Image src="/dfc_logo2.png" alt="Example Image" width={40} height={40} />
       <div className="flex">
-        <MdOutlineDehaze size={30} />
-        <div className="flex items-center">
+        <div
+          className="self-center  md:hidden cursor-pointer "
+          onClick={toggleNavOpen}
+        >
+          <MdOutlineDehaze size={30} />
+        </div>
+
+        {navOpen ? (
+          <div className="flex md:flex-col items-center">
+            <Link className="px-5" href="/">
+              Welcome
+            </Link>
+
+            <Link className="px-5" href="/chatbox">
+              Chat Box
+            </Link>
+
+            <Link className="px-5" href="/login">
+              Login
+            </Link>
+            <Link className="px-5" href="/register">
+              Register
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+        {/* <div className="flex items-center">
           <Link className="px-5" href="/">
             Welcome
           </Link>
@@ -24,7 +57,7 @@ function Nav_bar() {
           <Link className="px-5" href="/register">
             Register
           </Link>
-        </div>
+        </div> */}
       </div>
     </nav>
   );
