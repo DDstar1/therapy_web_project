7ecfeff7d26f24b4d145dae1aa0d5b8261f77fd4
DDstar1 <abhuluimendestiny@gmail.com>
2024-04-25 11:53:20 +0100

    ugvvggv

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 import { MdOutlineDehaze } from "react-icons/md";
 
 function Nav_bar() {
-  const [navOpen, setnavOpen] = useState(false);
+  const [navOpen, setnavOpen] = useState(true);
 
   const toggleNavOpen = (index: any) => {
     setnavOpen(!navOpen);
@@ -14,7 +14,7 @@ function Nav_bar() {
   return (
     <nav className="box-border z-40 flex justify-between p-2 overflow-hidden min-w-full fixed top-0 bg-gray-300 flex-grow">
       <Image src="/dfc_logo2.png" alt="Example Image" width={40} height={40} />
-      <div className="flex">
+      <div className="flex flex-col justify-center">
         <div
           className="self-center  md:hidden cursor-pointer "
           onClick={toggleNavOpen}
@@ -22,26 +22,27 @@ function Nav_bar() {
           <MdOutlineDehaze size={30} />
         </div>
 
-        {navOpen ? (
-          <div className="flex md:flex-col items-center">
-            <Link className="px-5" href="/">
-              Welcome
-            </Link>
+        <div
+          className={`flex fixed top-12 md:relative md:top-0 right-0 flex-col md:flex-row items-center bg-gray-300 transform duration-[200ms] h-fit overflow-hidden transition-all ${
+            navOpen ? "max-h-72" : "max-h-0"
+          }`}
+        >
+          <Link className="px-5 py-4 md:py-0" href="/">
+            Welcome
+          </Link>
 
-            <Link className="px-5" href="/chatbox">
-              Chat Box
-            </Link>
+          <Link className="px-5 py-4 md:py-0" href="/chatbox">
+            Chat Box
+          </Link>
 
-            <Link className="px-5" href="/login">
-              Login
-            </Link>
-            <Link className="px-5" href="/register">
-              Register
-            </Link>
-          </div>
-        ) : (
-          <></>
-        )}
+          <Link className="px-5 py-4 md:py-0" href="/login">
+            Login
+          </Link>
+          <Link className="px-5 py-4 md:py-0" href="/register">
+            Register
+          </Link>
+        </div>
+
         {/* <div className="flex items-center">
           <Link className="px-5" href="/">
             Welcome
