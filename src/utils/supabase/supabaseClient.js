b7bd99d60c56378ee727f6761a844dc6921890df
DDstar1 <abhuluimendestiny@gmail.com>
2024-05-04 14:09:12 +0100

    sdsd

--- src/utils/supabase/supabaseClient.js
+++ src/utils/supabase/supabaseClient.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://crivorhwoyglfvjeqldt.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNyaXZvcmh3b3lnbGZ2amVxbGR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQxMzQzOTksImV4cCI6MjAyOTcxMDM5OX0.bLkUITiXRyScD4XHrjqdyKu_1X2krglZaip4UXGtQxU";
 
 const supabaseClient = createClient(supabaseUrl, supabaseKey);
 
